2f4384aebc8e41202182da2645881d8cb6f33dd8
Krzysztof Kraszewski <krszwsk@gmail.com>
2017-03-09 20:42:27 +0100

    handling multiple currencies with struct array, not functional yet

--- Coiny/StatusMenuController.swift
+++ Coiny/StatusMenuController.swift
@@ -8,80 +8,91 @@
 
 import Cocoa
 
+struct Currency {
+  var symbol: String
+  var price: Double
+}
+
 class StatusMenuController: NSObject, PreferencesWindowDelegate {
-    @IBOutlet weak var statusMenu: NSMenu!
-    @IBOutlet weak var bitcoinPrice: NSMenuItem!
+  @IBOutlet weak var statusMenu: NSMenu!
+  @IBOutlet weak var bitcoinPrice: NSMenuItem!
     
-    let statusItem = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
-    let coinAPI = CoinAPI()
-    var timer = Timer()
-    let defaults = UserDefaults.standard
+  let statusItem = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
+  let coinAPI = CoinAPI()
+  var timer = Timer()
+  let defaults = UserDefaults.standard
     
-    var btcPrice: Double!
+  var btcPrice: Double!
+  
+  var prices: [Currency] = []
+  
+  var preferencesWindow: PreferencesWindow!
     
-    var preferencesWindow: PreferencesWindow!
-    
-    override func awakeFromNib() {
-        statusItem.title = "Fetching..."
-        statusItem.menu = statusMenu
+  override func awakeFromNib() {
+    statusItem.title = "Fetching..."
+    statusItem.menu = statusMenu
         
-        preferencesWindow = PreferencesWindow()
-        preferencesWindow.delegate = self
+    preferencesWindow = PreferencesWindow()
+    preferencesWindow.delegate = self
         
-        // User settings
-        var updateInterval: Double = defaults.double(forKey: "updateInterval")
-        updateInterval = updateInterval < 60 ? 60 : updateInterval
-        defaults.set(defaults.array(forKey: "currencies") ?? ["btc"], forKey: "currencies")
+    // User settings
+    var updateInterval: Double = defaults.double(forKey: "updateInterval")
+    updateInterval = updateInterval < 60 ? 60 : updateInterval
+    defaults.set(defaults.array(forKey: "currencies") ?? ["btc"], forKey: "currencies")
 
         
-        updatePrices()
-        timer = Timer.scheduledTimer(timeInterval: updateInterval, target: self, selector: #selector(self.updatePrices), userInfo: nil, repeats: true)
-    }
+    updatePrices()
+    timer = Timer.scheduledTimer(timeInterval: updateInterval, target: self, selector: #selector(self.updatePrices), userInfo: nil, repeats: true)
+  }
     
-    func updatePrices() {
-        coinAPI.fetchPrice("btc") { amount in
-            self.btcPrice = amount
-            self.updateView()
-        }
+  func updatePrices() {
+    let currencies = defaults.array(forKey: "currencies") as! [String]
+    for currency in currencies {
+      coinAPI.fetchPrice(currency) { amount in
+        self.prices.append(Currency(symbol: currency, price: amount))
+        NSLog("symbol: \(currency) amount: \(amount)")
+      }
     }
+    updateView()
+  }
     
-    func updateView() {
-        bitcoinPrice.title = "BTC: $\(convertPrice(btcPrice))"
-        statusItem.title = "$\(convertPrice(btcPrice))"
-    }
+  func updateView() {
+    //bitcoinPrice.title = "BTC: $\(convertPrice(btcPrice))"
+    //statusItem.title = "$\(convertPrice(btcPrice))"
+  }
     
-    func convertPrice(_ amount: Double) -> String {
-        if amount > 1000 && !defaults.bool(forKey: "showDecimals") {
-            return String(Int(round(amount)))
-        } else {
-            return String(amount)
-        }
+  func convertPrice(_ amount: Double) -> String {
+    if amount > 1000 && !defaults.bool(forKey: "showDecimals") {
+      return String(Int(round(amount)))
+    } else {
+      return String(amount)
     }
+  }
     
-    @IBAction func updateClicked(_ sender: NSMenuItem) {
-        updatePrices()
-    }
+  @IBAction func updateClicked(_ sender: NSMenuItem) {
+    updatePrices()
+  }
     
-    @IBAction func preferencesClicked(_ sender: NSMenuItem) {
-        preferencesWindow.showWindow(nil)
-    }
+  @IBAction func preferencesClicked(_ sender: NSMenuItem) {
+    preferencesWindow.showWindow(nil)
+  }
     
-    @IBAction func toggleShowDecimals(_ sender: NSMenuItem) {
-        defaults.set(!defaults.bool(forKey: "showDecimals"), forKey: "showDecimals")
-        sender.state = sender.state == 1 ? 0 : 1
-        updateView()
-    }
+  @IBAction func toggleShowDecimals(_ sender: NSMenuItem) {
+    defaults.set(!defaults.bool(forKey: "showDecimals"), forKey: "showDecimals")
+    sender.state = sender.state == 1 ? 0 : 1
+    updateView()
+  }
     
-    @IBAction func quitClicked(_ sender: NSMenuItem) {
-        timer.invalidate()
-        NSApplication.shared().terminate(self)
-    }
+  @IBAction func quitClicked(_ sender: NSMenuItem) {
+    timer.invalidate()
+    NSApplication.shared().terminate(self)
+  }
     
-    func preferencesDidClose() {
-        NSLog("App knows preferences were just closed.")
-    }
+  func preferencesDidClose() {
+    NSLog("App knows preferences were just closed.")
+  }
     
-    func showDecimalsStateChanged() {
-        updateView()
-    }
+  func showDecimalsStateChanged() {
+    updateView()
+  }
 }
