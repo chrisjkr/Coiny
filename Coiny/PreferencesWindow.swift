a2033a52a158dc775fcb6f0b5242f11f60869e31
Krzysztof Kraszewski <krszwsk@gmail.com>
2017-03-09 18:41:51 +0100

    displaying saved currencies in TableView

--- Coiny/PreferencesWindow.swift
+++ Coiny/PreferencesWindow.swift
@@ -9,70 +9,92 @@
 import Cocoa
 
 protocol PreferencesWindowDelegate {
-    func preferencesDidClose()
-    func showDecimalsStateChanged()
+  func preferencesDidClose()
+  func showDecimalsStateChanged()
 }
 
 class PreferencesWindow: NSWindowController, NSWindowDelegate {
     
-    var delegate: PreferencesWindowDelegate?
+  var delegate: PreferencesWindowDelegate?
 
-    @IBOutlet weak var intervalSlider: NSSlider!
-    @IBOutlet weak var showDecimals: NSButton!
-    @IBOutlet weak var currencyList: NSTextField!
-    @IBOutlet weak var addCurrencyText: NSTextField!
+  @IBOutlet weak var intervalSlider: NSSlider!
+  @IBOutlet weak var showDecimals: NSButton!
+  @IBOutlet weak var currencyList: NSTextField!
+  @IBOutlet weak var addCurrencyText: NSTextField!
+  @IBOutlet weak var currencyTable: NSTableView!
     
-    var defaults = UserDefaults.standard
+  var defaults = UserDefaults.standard
 
-    override var windowNibName: String! {
-        return "PreferencesWindow"
-    }
+  override var windowNibName: String! {
+    return "PreferencesWindow"
+  }
     
-    override func windowDidLoad() {
-        super.windowDidLoad()
+  override func windowDidLoad() {
+    super.windowDidLoad()
         
-        self.window?.center()
-        self.window?.makeKeyAndOrderFront(nil)
-        NSApp.activate(ignoringOtherApps: true)
+    self.window?.center()
+    self.window?.makeKeyAndOrderFront(nil)
+    NSApp.activate(ignoringOtherApps: true)
+    
+    currencyTable.delegate = self
+    currencyTable.dataSource = self
+    
+    updateView()
+  }
+  
+  func updateView() {
+    intervalSlider.doubleValue = defaults.double(forKey: "updateInterval")
+    showDecimals.state = defaults.bool(forKey: "showDecimals") ? 1 : 0
         
-        updateView()
-    }
+      
+  }
     
-    func updateView() {
-        intervalSlider.doubleValue = defaults.double(forKey: "updateInterval")
-        showDecimals.state = defaults.bool(forKey: "showDecimals") ? 1 : 0
+  @IBAction func intervalSliderUsed(_ sender: NSSliderCell) {
+      let event = NSApplication.shared().currentEvent
         
-        var currenciesString: String = ""
-        let currencies = defaults.array(forKey: "currencies")
-        for currency in currencies as! [String] {
-            currenciesString = currenciesString + "\n\(currency)"
-        }
-        currencyList.stringValue = currenciesString
-    }
+      if event?.type == NSEventType.leftMouseUp {
+          defaults.set(sender.doubleValue, forKey: "updateInterval")
+      }
+  }
     
-    @IBAction func intervalSliderUsed(_ sender: NSSliderCell) {
-        let event = NSApplication.shared().currentEvent
-        
-        if event?.type == NSEventType.leftMouseUp {
-            defaults.set(sender.doubleValue, forKey: "updateInterval")
-        }
-    }
-    
-    @IBAction func showDecimalsUsed(_ sender: NSButton) {
-        defaults.set(!defaults.bool(forKey: "showDecimals"), forKey: "showDecimals")
-        delegate?.showDecimalsStateChanged()
-    }
+  @IBAction func showDecimalsUsed(_ sender: NSButton) {
+      defaults.set(!defaults.bool(forKey: "showDecimals"), forKey: "showDecimals")
+      delegate?.showDecimalsStateChanged()
+  }
     
     
-    @IBAction func AddCurrency(_ sender: NSButton) {
-        if let currency = addCurrencyText?.stringValue {
-            var currencies = defaults.array(forKey: "currencies")
-            currencies?.append(currency)
-            defaults.set(currencies, forKey: "currencies")
-        }
+  @IBAction func AddCurrency(_ sender: NSButton) {
+      if let currency = addCurrencyText?.stringValue {
+          var currencies = defaults.array(forKey: "currencies")
+          currencies?.append(currency)
+          defaults.set(currencies, forKey: "currencies")
+      }
+  }
+    
+  func windowWillClose(_ notification: Notification) {
+      delegate?.preferencesDidClose()
+  }
+}
+
+extension PreferencesWindow: NSTableViewDataSource {
+  func numberOfRows(in tableView: NSTableView) -> Int {
+    return defaults.array(forKey: "currencies")?.count ?? 0
+  }
+}
+
+extension PreferencesWindow: NSTableViewDelegate {
+  func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+    
+    guard let item = defaults.array(forKey: "currencies")?[row] as? String else {
+      return nil
     }
     
-    func windowWillClose(_ notification: Notification) {
-        delegate?.preferencesDidClose()
+    if let cell = tableView.make(withIdentifier: "CurrencyCellID", owner: nil) as? NSTableCellView {
+      cell.textField?.stringValue = item
+      NSLog(item)
+      return cell
     }
+    
+    return nil
+  }
 }
